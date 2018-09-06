2e1bec5f97f612d141b4e452a8ca99c98c56daa3
wdhust <wdhust@qq.com>
2018-09-06 19:54:14 +0800

    Initial commit

---
+++ MyHomePage/WebFileUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor;
+using Microsoft.JSInterop;
+
+namespace MyHomePage
+{
+	public class WebFileUtil
+	{
+		public static Task<string> ShowPrompt(string message)
+		{
+			return JSRuntime.Current.InvokeAsync<string>("webFileUtil.showPrompt", message);
+		}
+
+		public static Task<string> ReadUploadedFileAsText(ElementRef inputElemRef)
+		{
+			return JSRuntime.Current.InvokeAsync<string>("webFileUtil.readUploadedFileAsText", inputElemRef);
+		}
+	}
+}
