a295a3d01e542f4e9972e1130c26682bef29e6f1
wdhust <wdhust@qq.com>
2018-09-08 03:54:06 +0800

    Test the read and write function with pure javascript

--- MyHomePage/WebFileUtil.cs
+++ MyHomePage/WebFileUtil.cs
@@ -7,31 +7,79 @@ using Microsoft.JSInterop;
 
 namespace MyHomePage
 {
-	public class WebFileUtil
-	{
-		public static Task<string> ShowPrompt(string message)
-		{
-			return JSRuntime.Current.InvokeAsync<string>("webFileUtil.showPrompt", message);
-		}
+    public class WebFileUtil
+    {
+        public static Task<string> ShowPrompt(string message)
+        {
+            return JSRuntime.Current.InvokeAsync<string>("webFileUtil.showPrompt", message);
+        }
 
-		public static Task<string> ReadUploadedFileAsText(ElementRef inputElemRef)
-		{
-			return JSRuntime.Current.InvokeAsync<string>("webFileUtil.readUploadedFileAsText", inputElemRef);
-		}
+        public static Task<string> ReadUploadedFileAsText(ElementRef inputElemRef)
+        {
+            return JSRuntime.Current.InvokeAsync<string>("webFileUtil.readUploadedFileAsText", inputElemRef);
+        }
 
-		public static Task WriteTextFileAndDownload(string fileName, string textContent)
-		{
-			return JSRuntime.Current.InvokeAsync<object>("webFileUtil.writeTextFileAndDownload", fileName, textContent);
-		}
+        public static Task WriteTextFileAndDownload(string fileName, string textContent)
+        {
+            return JSRuntime.Current.InvokeAsync<object>("webFileUtil.writeTextFileAndDownload", fileName, textContent);
+        }
 
-		public static Task<string[]> GetStringArray(string str, int count)
-		{
-			return JSRuntime.Current.InvokeAsync<string[]>("webFileUtil.getStringArray", str, count);
-		}
+        public static Task<string[]> GetStringArray(string str, int count)
+        {
+            return JSRuntime.Current.InvokeAsync<string[]>("webFileUtil.getStringArray", str, count);
+        }
 
-		public static Task<string[]> ReadLargeTextFile(ElementRef inputElemRef, int sliceSize = 8192)
-		{
-			return JSRuntime.Current.InvokeAsync<string[]>("webFileUtil.readLargeTextFile", inputElemRef, sliceSize);
-		}
-	}
+        public static Task<string[]> ReadLargeTextFile(ElementRef inputElemRef, int sliceSize = 8192)
+        {
+            return JSRuntime.Current.InvokeAsync<string[]>("webFileUtil.readLargeTextFile", inputElemRef, sliceSize);
+        }
+    }
+
+    public class WebFile
+    {
+        public static void InitBuffer(int length)
+        {
+            ((IJSInProcessRuntime) JSRuntime.Current).Invoke<object>("webFile.initBuffer", length);
+        }
+
+        public static void ClearBuffer()
+        {
+            ((IJSInProcessRuntime)JSRuntime.Current).Invoke<object>("webFile.clearBuffer");
+        }
+
+        public static int GetBufferLength()
+        {
+            return ((IJSInProcessRuntime) JSRuntime.Current).Invoke<int>("webFile.getBufferLength");
+        }
+
+        public static Task FillBuffer(string str)
+        {
+            return JSRuntime.Current.InvokeAsync<object>("webFile.fillBuffer", str);
+        }
+
+        public static Task<string> GetBufferData(int index)
+        {
+            return JSRuntime.Current.InvokeAsync<string>("webFile.getBufferData", index);
+        }
+
+        public static Task SetBufferData(int index, string str)
+        {
+            return JSRuntime.Current.InvokeAsync<object>("webFile.setBufferData", index, str);
+        }
+
+        public static Task<int> ReadFileToBuffer(ElementRef fileElemRef, int sliceSize = 8192)
+        {
+            return JSRuntime.Current.InvokeAsync<int>("webFile.readFileToBuffer", fileElemRef, sliceSize);
+        }
+
+        public static Task WriteBufferToFile(string fileName)
+        {
+            return JSRuntime.Current.InvokeAsync<object>("webFile.writeBufferToFile", fileName);
+        }
+
+        public static bool HasSelectedFile(ElementRef fileElemRef)
+        {
+            return ((IJSInProcessRuntime)JSRuntime.Current).Invoke<bool>("webFile.hasSelectedFile", fileElemRef);
+        }
+    }
 }
---
+++ MyHomePage/wwwroot/script/webFile.js
@@ -0,0 +1,103 @@
+﻿window.webFile = {
+    // 
+    // temporary buffer for large data exchange
+    // 
+    _tempBuffer_: [],
+
+    initBuffer: (length) => {
+        _tempBuffer_ = Array(length);
+    },
+
+    clearBuffer: () => {
+        _tempBuffer_ = [];
+    },
+
+    fillBuffer: (str) => {
+        _tempBuffer_.push(str);
+    },
+
+    getBufferLength: () => {
+        return _tempBuffer_.length;
+    },
+
+    getBufferData: (index) => {
+        return _tempBuffer_[index];
+    },
+
+    setBufferData: (index, str) => {
+        _tempBuffer_[index] = str;
+    },
+
+    //
+    // file element
+    //
+    hasSelectedFile: (fileElemRef) => {
+        const file = fileElemRef.files[0];
+        return (!file) ? false : true;
+    },
+
+    //
+    // read data into buffer
+    //
+    readFileToBuffer: (fileElemRef, sliceSize) => {
+        const file = fileElemRef.files[0];
+        if (!file) {
+            return 0;
+        }
+
+        _tempBuffer_ = [];
+
+        const reader = new FileReader();
+        return new Promise((resolve, reject) => {
+            reader.onerror = () => {
+                reader.abort();
+                reject(new DOMException("Error occured while parsing file."));
+            };
+
+            //
+            const fileSize = file.size;
+            //const chunkList = [];
+
+            let offset = 0;
+            reader.onloadend = (e) => {
+                if (e.target.readyState === FileReader.DONE) {
+                    const chunk = e.target.result;
+                    //chunkList.push(chunk);
+                    _tempBuffer_.push(chunk);
+
+                    if (offset < fileSize) {
+                        offset += sliceSize;
+                        const blob = file.slice(offset, offset + sliceSize);
+                        reader.readAsText(blob);
+                    } else {
+                        console.log("Read large file complete !");
+                        //resolve(chunkList);
+                        resolve(0);
+                    };
+                }
+            };
+
+            const blob = file.slice(offset, offset + sliceSize);
+            reader.readAsText(blob);
+        });
+    },
+
+    //
+    // write buffer data to file
+    //
+    writeBufferToFile: (fileName) => {
+        const textContent = _tempBuffer_.join("");
+        const blob = new Blob([textContent], { type: "text/csv" });
+        if (window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            const elem = window.document.createElement("a");
+            elem.href = window.URL.createObjectURL(blob);
+            elem.download = fileName;
+
+            window.document.body.appendChild(elem);
+            elem.click();
+            window.document.body.removeChild(elem);
+        }
+    }
+}
